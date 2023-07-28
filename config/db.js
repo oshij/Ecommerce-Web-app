b17d5319fb4912de02f3b769d083cd4cbcd3afe8
oshij <oshijtrivedi@gmail.com>
2023-07-28 09:06:40 +0530

    mongodb bug removed

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require('colors')
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(process.env.Mongo_URL)
+        const conn = await mongoose.connect(process.env.MONGO_URL)
         console.log(`Connected To Mongodb Database ${conn.connection.host}`)
     }catch(error){
         console.log(`Error in Mongodb ${error}`.bgRed.white)

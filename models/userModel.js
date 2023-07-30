cfbdef6e6284618ea0694208e1462f1c862c7f9c
oshij <oshijtrivedi@gmail.com>
2023-07-30 17:05:18 +0530

    register Model is ready

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,32 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-
-})
+    name:{
+        type: String,
+        required: true,
+        trim:true // for removine jitna bhi white space
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true // ek email se ek user hi enter kare ga ,there for we are using unique true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: Number,
+        default: 0
+    }
+    
+},{ timestamps: true }); // jab bhi new user create hooga  to created time register hoojaye gaa);
 module.exports = mongoose.model('users',userSchema)
---
+++ routes/authRoute.js
@@ -0,0 +1,3 @@
+const express = required('express')
+
+//router object

ada3cbfc1f1a231a9375a844fbd8355b44a02b36
oshij <oshijtrivedi@gmail.com>
2023-08-03 23:39:36 +0530

    new commit

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,3 +1,12 @@
 const express = required('express')
+const
 
 //router object
+
+const router = express.Router()
+
+//routing
+//Register || Method post
+
+router.post('/register', registerController)
+export default router
